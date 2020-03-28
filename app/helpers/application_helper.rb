e20716db19c09c55ef8224f1aa8879af8b78e5cb
Evan Duncan <evan.e.duncan@gmail.com>
2020-03-27 21:06:19 -0500

    Configure error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def devise_flash_to_bootstrap(type)
+    case type.to_sym
+    when :alert
+      :danger
+    when :notice
+      :success
+    else
+      :info
+    end
+  end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('@popperjs/core')
-require('jquery')
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -17,3 +14,7 @@ require('jquery')
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import 'bootstrap'
+
+document.addEventListener("turbolinks:load", () => $('[data-toggle="tooltip"]').tooltip());
